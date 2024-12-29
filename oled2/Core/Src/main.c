20a674d6f21543038e51bc3e68a34bd2a6b9f6eb
sasi <2975547871@qq.com>
2024-12-29 18:14:12 +0800

    提交文件

--- oled2/Core/Src/main.c
+++ oled2/Core/Src/main.c
@@ -6,12 +6,13 @@
   ******************************************************************************
   * @attention
   *
-  * Copyright (c) 2024 STMicroelectronics.
-  * All rights reserved.
+  * <h2><center>&copy; Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.</center></h2>
   *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
   *
   ******************************************************************************
   */
@@ -21,90 +22,125 @@
 #include "i2c.h"
 #include "tim.h"
 #include "gpio.h"
-
+ 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "stm32_u8g2.h"
+#include "test.h"
 /* USER CODE END Includes */
-
+ 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
-
+ 
 /* USER CODE END PTD */
-
+ 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-
 /* USER CODE END PD */
-
+ 
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
-
+ 
 /* USER CODE END PM */
-
+ 
 /* Private variables ---------------------------------------------------------*/
-
+ 
 /* USER CODE BEGIN PV */
-
+ 
 /* USER CODE END PV */
-
+ 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
-
+ 
 /* USER CODE END PFP */
-
+ 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
+ 
 /* USER CODE END 0 */
-
+ 
 /**
   * @brief  The application entry point.
   * @retval int
   */
+/*µ±*/
+static const unsigned char  dang[] ={
+0x80,0x00,0x84,0x10,0x88,0x10,0x90,0x08,0x90,0x04,0x80,0x00,0xFE,0x1F,0x00,0x10,0x00,0x10,0x00,0x10,0xFC,0x1F,0x00,0x10,0x00,0x10,0x00,0x10,0xFE,0x1F,0x00,0x10};
+/*Ê±*/
+static const unsigned char shi[] ={
+0x00,0x10,0x00,0x10,0x3E,0x10,0x22,0x10,0xA2,0x7F,0x22,0x10,0x22,0x10,0x3E,0x10,0x22,0x11,0x22,0x12,0x22,0x12,0x22,0x10,0x3E,0x10,0x22,0x10,0x00,0x14,0x00,0x08};
+/*Ã÷*/
+static const unsigned char ming[] ={
+0x00,0x00,0x00,0x3F,0x3E,0x21,0x22,0x21,0x22,0x21,0x22,0x3F,0x3E,0x21,0x22,0x21,0x22,0x21,0x22,0x3F,0x3E,0x21,0x22,0x21,0x80,0x20,0x80,0x20,0x40,0x28,0x20,0x10};
+/*ÔÂ*/	
+static const unsigned char yue[]={
+0x00,0x00,0xF8,0x1F,0x08,0x10,0x08,0x10,0x08,0x10,0xF8,0x1F,0x08,0x10,0x08,0x10,0x08,0x10,0xF8,0x1F,0x08,0x10,0x08,0x10,0x04,0x10,0x04,0x10,0x02,0x14,0x01,0x08};
+/*ÔÚ*/	
+static const unsigned char zai[]={
+0x40,0x00,0x40,0x00,0x20,0x00,0xFF,0x7F,0x10,0x00,0x10,0x02,0x08,0x02,0x0C,0x02,0xEA,0x3F,0x09,0x02,0x08,0x02,0x08,0x02,0x08,0x02,0x08,0x02,0xF8,0x7F,0x08,0x00};
+	
 int main(void)
 {
-
   /* USER CODE BEGIN 1 */
-
+ 
   /* USER CODE END 1 */
-
+ 
   /* MCU Configuration--------------------------------------------------------*/
-
+ 
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
-
+ 
   /* USER CODE BEGIN Init */
-
+ 
   /* USER CODE END Init */
-
+ 
   /* Configure the system clock */
   SystemClock_Config();
-
+ 
   /* USER CODE BEGIN SysInit */
-
+ 
   /* USER CODE END SysInit */
-
+ 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_I2C1_Init();
+  MX_I2C2_Init();
   MX_TIM1_Init();
   /* USER CODE BEGIN 2 */
-
+  u8g2_t u8g2;
+  u8g2Init(&u8g2);	
   /* USER CODE END 2 */
-
+ 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+	
+	
+	unsigned int x=16;
   while (1)
   {
     /* USER CODE END WHILE */
-
+ 
     /* USER CODE BEGIN 3 */
+ 
+     if(x<=128)
+		{
+			x++;//????,??????x???????????
+		}
+		else if(x>128)//??
+		{
+			x=0;
+		}
+				u8g2_SetFont(&u8g2,u8g2_font_ncenB12_tf);//??????
+				u8g2_DrawXBMP(&u8g2,x,0,16,16,dang);
+				u8g2_DrawXBMP(&u8g2,x+16,0,16,16,shi);
+				u8g2_DrawXBMP(&u8g2,x+32,0,16,16,ming);
+				u8g2_DrawXBMP(&u8g2,x+48,0,16,16,yue);
+				u8g2_DrawXBMP(&u8g2,x+64,0,16,16,zai);
+				u8g2_SendBuffer(&u8g2);
   }
   /* USER CODE END 3 */
 }
-
+ 
 /**
   * @brief System Clock Configuration
   * @retval None
@@ -113,7 +149,7 @@ void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-
+ 
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
@@ -128,7 +164,6 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-
   /** Initializes the CPU, AHB and APB buses clocks
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
@@ -137,17 +172,17 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-
+ 
   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
   {
     Error_Handler();
   }
 }
-
+ 
 /* USER CODE BEGIN 4 */
-
+ 
 /* USER CODE END 4 */
-
+ 
 /**
   * @brief  This function is executed in case of error occurrence.
   * @retval None
@@ -162,7 +197,7 @@ void Error_Handler(void)
   }
   /* USER CODE END Error_Handler_Debug */
 }
-
+ 
 #ifdef  USE_FULL_ASSERT
 /**
   * @brief  Reports the name of the source file and the source line number
@@ -179,3 +214,5 @@ void assert_failed(uint8_t *file, uint32_t line)
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
+ 
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
